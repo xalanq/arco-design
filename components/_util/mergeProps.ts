4360d524b7577008ae020b895fe0b00a3897639c
xalanq <xalanq@gmail.com>
2022-10-07 16:44:12 +0800

    perf: 优化 _util/mergeProps.ts 里 mergeProps 的性能

--- components/_util/mergeProps.ts
+++ components/_util/mergeProps.ts
@@ -3,15 +3,11 @@ export default function mergeProps<PropsType>(
   defaultProps: Partial<PropsType>,
   globalComponentConfig: PropsType
 ): PropsType {
-  const _defaultProps = { ...defaultProps, ...globalComponentConfig };
-  const props = { ...componentProps };
-
-  // https://github.com/facebook/react/blob/cae635054e17a6f107a39d328649137b83f25972/packages/react/src/ReactElement.js#L312
-  for (const propName in _defaultProps) {
-    if (props[propName] === undefined) {
-      props[propName] = _defaultProps[propName];
+  const props = { ...defaultProps, ...globalComponentConfig };
+  Object.entries(componentProps).forEach(([propName, propValue]) => {
+    if (propValue !== undefined) {
+      props[propName] = propValue;
     }
-  }
-
+  });
   return props;
 }
